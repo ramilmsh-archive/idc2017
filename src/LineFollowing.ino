7b42561522522eda8c807d3d28c82108b60fe3ee
Ramil Shaymardanov <ramilmsh@MacBook-Pro-5.local>
2017-03-20 15:33:34 -0400

    ...

--- src/LineFollowing.ino
+++ src/LineFollowing.ino
@@ -21,63 +21,63 @@ int t = 50;
 Servo left, right;
 
 void setup() {
-  // Starting serial
-  Serial.begin(9600);
-  Xbee.begin(9600);
-  while (!Serial);
+    // Starting serial
+    Serial.begin(9600);
+    Xbee.begin(9600);
+    while (!Serial);
 
-  left.attach(13);
-  right.attach(12);
+    left.attach(13);
+    right.attach(12);
 
-  // Switching QTI on
-  pinMode(3, OUTPUT);
-  digitalWrite(3, HIGH);
-  pinMode(6, OUTPUT);
-  digitalWrite(6, HIGH);
+    // Switching QTI on
+    pinMode(3, OUTPUT);
+    digitalWrite(3, HIGH);
+    pinMode(6, OUTPUT);
+    digitalWrite(6, HIGH);
 }
 
 void loop() {
 
-  ls1 = RCTime(5);
-  ls2 = RCTime(2);
+    ls1 = RCTime(5);
+    ls2 = RCTime(2);
 
-  Serial.print(ls2);
-  Serial.print(' ');
-  Serial.println(ls1);
+    Serial.print(ls2);
+    Serial.print(' ');
+    Serial.println(ls1);
 
-  if ((ls1 > t) && (ls2 > t))
+    if ((ls1 > t) && (ls2 > t))
     speed(0);
-  else if (ls1 > t && ls2 < t)
+    else if (ls1 > t && ls2 < t)
     speed(-.5, .5);
-  else if (ls1 < t && ls2 > t)
+    else if (ls1 < t && ls2 > t)
     speed(.5, -.5);
-  else
+    else
     speed(1);
 }
 
 // Set speed between -1 and 1
 // l - left, r - right
 void speed(double l, double r) {
-  // TODO add calibration ?
-  left.write(1500 + 200 * l);
-  right.write(1500 - 100 * r);
-  delay(l != r ? 100 : 50);
+    // TODO add calibration ?
+    left.write(1500 + 200 * l);
+    right.write(1500 - 100 * r);
+    delay(l != r ? 100 : 50);
 }
 
 // Set forward speed
 void speed(double s) {
-  speed(s, s);
+    speed(s, s);
 }
 
 long RCTime(int pin) {
-  long duration = 0;
-  pinMode(pin, OUTPUT);
-  digitalWrite(pin, HIGH);
-  delay(1);
-  pinMode(pin, INPUT);
-  digitalWrite(pin, LOW);
-  while (digitalRead(pin))
+    long duration = 0;
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, HIGH);
+    delay(1);
+    pinMode(pin, INPUT);
+    digitalWrite(pin, LOW);
+    while (digitalRead(pin))
     duration++;
 
-  return duration;
+    return duration;
 }
