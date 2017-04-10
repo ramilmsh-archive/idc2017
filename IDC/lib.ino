ab94c4d6d58cddbfbcf56532039b11ec99370a1e
Ramil Shaymardanov <ramilmsh@MacBook-Pro-5.local>
2017-04-09 23:41:48 -0400

    adding comments

--- IDC/lib.ino
+++ IDC/lib.ino
@@ -3,7 +3,6 @@
 
 #define Xbee_Rx 53
 #define Xbee_Tx 51
-#define Xbee_5V 3
 
 #define LCD_Tx 10
 
@@ -21,6 +20,7 @@ int total_score;
 // Variable that keeps checks if all the data was received
 byte finished = 0;
 
+// Starting Xbee
 void setup_Xbee() {
   // Powering up the Xbee
   pinMode(Xbee_5V, OUTPUT);
@@ -41,35 +41,53 @@ void setup_LCD() {
   LCD.write(12);
 }
 
+// Printing string
 void write(char* s) {
   LCD.write(12);
   LCD.write(s);
 }
 
+// Printing an integer
 void write(int x) {
   LCD.write(12);
   LCD.print(x);
 }
 
+// Printing a character
 void write(byte x) {
   LCD.write(12);
   LCD.print(x, BIN);
 }
 
+// Calculating score
 int calc_score() {
   int score = 0;
+  // Removing botnumber from data and writing it to the variable
   group[BOT_NUMBER] = data & 31;
+
+  // Chaser:
+  //  if snitch is caught - 1
+  //  if not - 0
   score += group[0] * 150;
 
+  // Seekers:
+  //  find intersection of both and add 10 points for each
+  //  intersection found
+  //
+  // _t keeps all hashmarks that have intersection
   byte _t = group[1] & group[2];
   for (int i = 0; i < 5; ++i)
+    // check if there is anything at a hashmark
+    // if there is - add 10 points
+    // if not - add 0 points
     score += (((1 << i) & _t) != 0) * 10;
 
+  // Beaters:
+  //  same as Seeker
   _t = group[3] & group[4];
   for (int i = 0; i < 5; ++i)
     score += (((1 << i) & _t) != 0) * 10;
 
-  // TODO remove
   LCD.write(12);
   LCD.print(score);
   LCD.write(13);
@@ -82,19 +100,32 @@ void _send() {
   int count = 0;
 
   while (1) {
+    // if not all the info has been received and there is
+    // incoming signal
     if ((finished != 31) && Xbee.available()) {
+      // reading the character
       byte incoming = Xbee.read(),
+           // getting the number of the bot (first three bits)
            bot = incoming >> 5;
 
       Serial.println(incoming);
 
+      // getting only the info, removing the bot number
       incoming = incoming & 31;
       group[bot] = incoming;
 
+      // recording that this bot has been received already
       finished = finished | (1 << bot);
+      
+      // calculating the score and displaying it
       total_score = calc_score();
       write(total_score);
     }
+    
+    // Sending data every 50 ms
+    // !!! IMPORTANT !!!
+    // DO NOT USE Xbee.print
+    // Xbee write sends raw data, which is what we need
     Xbee.write(data);
     delay(50);
   }
